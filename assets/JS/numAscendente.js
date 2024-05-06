17e74891a338dfdf81c89c09fb46fa59bc97288c
sr-roboto <marcoslopez123.lu@gmail.com>
2024-05-05 23:26:10 -0300

    termine el codigo de numAscendente utilizacndo la funcion sort, comparando con la funcion comparar cada uno de los elementos del array numeros

--- assets/JS/numAscendente.js
+++ assets/JS/numAscendente.js
@@ -1,9 +1,25 @@
 let nro1 = parseInt(prompt("Ingrese el primer numero"));
 let nro2 = parseInt(prompt("Ingrese el segundo numero"));
 let nro3 = parseInt(prompt("Ingrese el tercer numero"));
-let numeros = [nro1, nro2, nro3];
+let numeros = [];
 
-numeros.sort() //con el metodo sort ordenamos los elementos de un arreglo
-numeros.reverse() //con el metodo reverse invierte¿imos el orden de los elementos de un array
+numeros.push(nro1)
+numeros.push(nro2)
+numeros.push(nro3)
 
-document.write(numeros);
+function comparar(a, b) { // una funcion que toma dos parametros del array y los compara devolviendo un valor 1 -1 0
+    return a - b; // con esto ordenamos los elementos del array en orden ascendente
+}
+
+numeros.sort(comparar); //con el metodo sort ordenamos los elementos de un arreglo
+
+document.write(numeros)
+
+
+
+
+
+
+
+
+
